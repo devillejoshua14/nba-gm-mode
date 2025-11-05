ba76b6f0f5b4a4e6eb82ed9e664494681c11d37b
Joshua DeVille <devillejoshua14@gmail.com>
2025-11-05 14:41:42 -0500

    Added background to main app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import PlayerList from "./components/PlayerList";
-/*import SyncButton from "./components/SyncButton";*/
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "2rem" }}>
-      <PlayerList />
+    // 🎨 Step 3: Background Polish Wrapper
+    <div
+      style={{
+        backgroundColor: "#0d1117",
+        minHeight: "100vh",
+        padding: "2rem",
+        color: "white",
+      }}
+    >
+      <div style={{ textAlign: "center", marginTop: "2rem" }}>
+        <PlayerList />
+      </div>
     </div>
   );
 }
 
 export default App;
-
